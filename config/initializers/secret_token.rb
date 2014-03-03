fcb8f0371d79a06e09637a0c368f894d918a9b5e
Carlos Interone <carlos@mostlyclicks.com>
2014-03-02 22:34:29 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fp04::Application.config.secret_key_base = 'd84e6005504a5d9e0bcdec8dc40f8de9a1127f053dbd7089e55905e257cd7841ccae3f3cd73ca6c6217895052fc2536e03eafd60c182f21017e1ac1ed7c98fe4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fp04::Application.config.session_store :cookie_store, key: '_fp_04_session'
